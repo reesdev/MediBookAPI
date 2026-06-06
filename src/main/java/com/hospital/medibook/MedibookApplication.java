e6fff63f48bf0799abf754ab0ecbad9002311655
Riski <reesky.it@gmail.com>
2026-06-06 20:21:24 +0700

    inisialisasi project spring boot pake maven

---
+++ src/main/java/com/hospital/medibook/MedibookApplication.java
@@ -0,0 +1,12 @@
+package com.hospital.medibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedibookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedibookApplication.class, args);
+    }
+}
